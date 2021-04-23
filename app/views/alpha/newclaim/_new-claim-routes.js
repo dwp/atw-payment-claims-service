97698dc75a31de1d5d52b037d18e62e0d83788a4
George Herbert <georgeherby@hotmail.com>
2021-04-23 14:01:14 +0100

    Tweaking stucture (#1)
    Co-authored-by: George Herbert <george.herbert@engineering.digital.dwp.gov.uk>

--- app/views/alpha/newclaim/_new-claim-routes.js
+++ app/views/alpha/newclaim/_new-claim-routes.js
@@ -1,41 +1,48 @@
-const express = require('express')
-const router = express.Router()
+module.exports = function (folderForViews, urlPrefix, router) {
+  // Load Portal
+  router.get('/portal', function (req, res) {
+    res.render(`./${folderForViews}/newclaim/portal`)
+  });
 
-// Branching
-router.post('/alpha/newclaim/test-answer', function (req, res) {
+  // Branching
+  router.get('/newclaim/new-claim', function (req, res) {
+    res.render(`./${folderForViews}/newclaim/new-claim`)
+  });
 
+  router.post('/newclaim/test-answer', function (req, res) {
+    const test = req.session.data['test']
 
+    if (test === 'Yes') {
 
-  const test = req.session.data['test']
+      res.redirect(`/${urlPrefix}/newclaim/special-aids-and-equipment`)
+    } else if (test === 'No') {
 
-  if (test === 'Yes') {
+      res.redirect(`/${urlPrefix}/newclaim/test-2`)
+    }
+  });
 
-    res.redirect('/alpha/newclaim/special-aids-and-equipment')
-  } else if (test === 'No'){
+  router.get('/newclaim/special-aids-and-equipment', function (req, res) {
+    res.render(`./${folderForViews}/newclaim/special-aids-and-equipment`)
+  });
 
-    res.redirect('/alpha/newclaim/test-1')
-  }
-})
+  router.get('/newclaim/test-2', function (req, res) {
+    res.render(`./${folderForViews}/newclaim/test-2`)
+  });
 
+  router.get('/newclaim/before-you-continue', function (req, res) {
+    res.render(`./${folderForViews}/newclaim/before-you-continue`)
+  });
 
+  //aids and equipment
+  router.post('newclaim/aids-and-equipment-answer', function (req, res) {
+    const aids = req.session.data['aids-and-equipment']
 
-//aids and equipment
-router.post('/alpha/newclaim/aids-and-equipment-answer', function (req, res) {
-
-
-
-  const aids = req.session.data['aids-and-equipment']
-
-  if (aids === 'Yes') {
-    console.log('yes')
-    res.redirect('/alpha/newclaim/before-you-continue')
-  } else if (aids === 'No'){
-    console.log('no')
-    res.redirect('/alpha/newclaim/test-1')
-  }
-})
-
-
-
-
-module.exports = router
+    if (aids === 'Yes') {
+      console.log('yes')
+      res.redirect(`/${urlPrefix}/newclaim/before-you-continue`)
+    } else if (aids === 'No') {
+      console.log('no')
+      res.redirect(`/${urlPrefix}/newclaim/test-2`)
+    }
+  });
+}
