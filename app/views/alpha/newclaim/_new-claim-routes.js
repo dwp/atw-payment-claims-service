8ba2c43065ca91fe591cabf8289114c041487f8b
owen-bennett123 <owen.bennett@engineering.digital.dwp.gov.uk>
2021-04-27 17:54:11 +0100

    HTO 932 - building new claims journey

--- app/views/alpha/newclaim/_new-claim-routes.js
+++ app/views/alpha/newclaim/_new-claim-routes.js
@@ -1,48 +1,86 @@
 module.exports = function (folderForViews, urlPrefix, router) {
   // Load Portal
-  router.get('/portal', function (req, res) {
-    res.render(`./${folderForViews}/newclaim/portal`)
-  });
 
-  // Branching
-  router.get('/newclaim/new-claim', function (req, res) {
-    res.render(`./${folderForViews}/newclaim/new-claim`)
-  });
+
 
   router.post('/newclaim/test-answer', function (req, res) {
     const test = req.session.data['test']
-
-    if (test === 'Yes') {
-
+    if (test === 'New') {
       res.redirect(`/${urlPrefix}/newclaim/special-aids-and-equipment`)
-    } else if (test === 'No') {
-
+    } else if (test === 'Repeat') {
       res.redirect(`/${urlPrefix}/newclaim/test-2`)
     }
   });
 
-  router.get('/newclaim/special-aids-and-equipment', function (req, res) {
-    res.render(`./${folderForViews}/newclaim/special-aids-and-equipment`)
-  });
 
-  router.get('/newclaim/test-2', function (req, res) {
-    res.render(`./${folderForViews}/newclaim/test-2`)
-  });
 
-  router.get('/newclaim/before-you-continue', function (req, res) {
-    res.render(`./${folderForViews}/newclaim/before-you-continue`)
-  });
+// Information on how to make a get
 
-  //aids and equipment
-  router.post('newclaim/aids-and-equipment-answer', function (req, res) {
+  // router.get('/newclaim/test-2', function (req, res) {
+  //   res.render(`./${folderForViews}/newclaim/test-2`)
+  // });
+  // router.get('/newclaim/before-you-continue', function (req, res) {
+  //   res.render(`./${folderForViews}/newclaim/before-you-continue`)
+  // });
+  //
+  //
+  // //aids and equipment
+  //
+  // // get
+  // router.get('/newclaim/special-aids-and-equipment', function (req, res) {
+  //   res.render(`./${folderForViews}/newclaim/special-aids-and-equipment`)
+  // });
+
+ //post - aids and equipment answer
+  router.post('/newclaim/aids-and-equipment-answer', function (req, res) {
     const aids = req.session.data['aids-and-equipment']
 
     if (aids === 'Yes') {
-      console.log('yes')
+
       res.redirect(`/${urlPrefix}/newclaim/before-you-continue`)
     } else if (aids === 'No') {
-      console.log('no')
+
       res.redirect(`/${urlPrefix}/newclaim/test-2`)
     }
   });
+
+  //post - who purchased the equipment
+   router.post('/newclaim/purchase-answer', function (req, res) {
+     const purchase = req.session.data['purchase-equipment']
+
+     if (purchase === 'Me') {
+       res.redirect(`/${urlPrefix}/newclaim/description`)
+     } else if (aids === 'My-employer') {
+       res.redirect(`/${urlPrefix}/newclaim/test-2`)
+     } else if (aids === 'Someone-else') {
+       res.redirect(`/${urlPrefix}/newclaim/test-2`)
+     }
+   });
+
+   //post - additional equipement purchased?
+    router.post('/newclaim/additional-equipment-answer', function (req, res) {
+      const equipment = req.session.data['additional-equipment']
+
+      if (equipment === 'No') {
+        res.redirect(`/${urlPrefix}/newclaim/information-about-the-purchaser`)
+      } else if (equipment === 'Yes') {
+        res.redirect(`/${urlPrefix}/newclaim/test-2`)
+      }
+    });
+
+    //post - Digital upload
+     router.post('/newclaim/digital-upload-answer', function (req, res) {
+       const upload = req.session.data['digital-upload']
+
+       if (upload === 'Yes') {
+         res.redirect(`/${urlPrefix}/newclaim/receipt-upload`)
+       } else if (equipment === 'No') {
+         res.redirect(`/${urlPrefix}/newclaim/test-2`)
+       }
+     });
+
+
+
+
+
 }
