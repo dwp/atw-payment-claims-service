4612b9092fc7d77bdee8b5b40587d5eec1687608
owen-bennett123 <owen.bennett@engineering.digital.dwp.gov.uk>
2021-05-27 16:16:19 +0100

    new portal view and card

--- app/routes/alpha.js
+++ app/routes/alpha.js
@@ -5,5 +5,7 @@ const folderWithinViews = 'alpha'
 const urlPrefix = 'alpha'
 
 require(`../views/${folderWithinViews}/newclaim/_new-claim-routes`)(folderWithinViews, urlPrefix, router)
+require(`../views/${folderWithinViews}/employer-countersign/_employer-countersign-routes`)(folderWithinViews, urlPrefix, router)
+
 
 module.exports = router
---
+++ app/views/alpha/employer-countersign/_employer-countersign-routes.js
@@ -0,0 +1,15 @@
+module.exports = function (folderForViews, urlPrefix, router) {
+  // Load Portal
+
+  // post - aids and equipment answer
+  router.post('/employer-countersign/employer-countersign-answer', function (req, res) {
+    const employer = req.session.data['employer-agree']
+
+    if (employer === 'Yes') {
+      res.redirect(`/${urlPrefix}/employer-countersign/declaration`)
+    } else if (employer === 'No') {
+      res.redirect(`/${urlPrefix}/employer-countersign/send-back`)
+    }
+  })
+
+}
