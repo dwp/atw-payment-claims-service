3cc0cd065ee47c840214b5024d6d250e8343f755
owen-bennett123 <owen.bennett@engineering.digital.dwp.gov.uk>
2021-06-17 16:18:11 +0100

    fix plz

--- app/routes/alpha.js
+++ app/routes/alpha.js
@@ -6,6 +6,7 @@ const urlPrefix = 'alpha'
 
 require(`../views/${folderWithinViews}/_portal-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/equipment-and-adaptations/_equipment-and-adaptations-routes`)(folderWithinViews, urlPrefix, router)
+require(`../views/${folderWithinViews}/equipment-and-adaptations-vi/_equipment-and-adaptations-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/employer-countersign/_employer-countersign-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/support-worker/_support-worker-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/travel-to-work/_travel-to-work-routes`)(folderWithinViews, urlPrefix, router)
