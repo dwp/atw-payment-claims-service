707c60e281ea086c5bac51c46621eeadae8d2288
owen-bennett123 <owen.bennett@engineering.digital.dwp.gov.uk>
2022-01-12 11:21:40 +0000

    travel-in-work journey

--- app/routes/alpha.js
+++ app/routes/alpha.js
@@ -18,6 +18,7 @@ require(`../views/${folderWithinViews}/travel-to-work/_travel-to-work-routes`)(f
 require(`../views/${folderWithinViews}/travel-to-work-dev/_travel-to-work-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/travel-in-work/_travel-in-work-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/travel-in-work-dev/_travel-in-work-routes`)(folderWithinViews, urlPrefix, router)
+require(`../views/${folderWithinViews}/travel-in-work-devv2/_travel-in-work-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/travel-to-work-old/_travel-to-work-routes`)(folderWithinViews, urlPrefix, router)
 require(`../views/${folderWithinViews}/multiple-award-elements/_multiple-award-routes`)(folderWithinViews, urlPrefix, router)
 
